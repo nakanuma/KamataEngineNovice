5d975832174ff40786f29301fde766320ba957d2
k023g0121desktop <k023g0121@g.neec.ac.jp>
2023-05-03 16:04:33 +0900

    背景が動かない

--- main.cpp
+++ main.cpp
@@ -12,6 +12,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	//画像読み込み
+	int bgGH = Novice::LoadTexture("./images/bg.png");
+
+	//シーンを管理する
+	enum Scene {
+		TITLE,
+		GAME,
+		RESULT
+	};
+
+	//アニメーション背景のY座標
+	int bg1Y = 0;
+	int bg2Y = -720;
+
+
+	Scene scene = TITLE;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -25,6 +42,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		switch (scene) {
+		case TITLE:
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				scene = GAME;
+			}
+			break;
+		case GAME:
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				scene = RESULT;
+				bg2Y++;
+				bg1Y++;
+				if (bg2Y >= 0 && bg1Y >= 720) {
+					bg2Y = -720;
+					bg1Y = 0;
+				}
+			}
+			break;
+		case RESULT:
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				scene = TITLE;
+			}
+			break;
+
+		}
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +73,43 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		switch (scene) {
+		case TITLE:
+			Novice::DrawBox(
+				0, 0, 
+				1280, 720, 
+				0.0f, 
+				0xEFC1C1FF, 
+				kFillModeSolid
+			);
+			break;
+		case GAME:
+			Novice::DrawSprite(
+				0, bg2Y, 
+				bgGH, 
+				1, 1, 
+				0.0f, 
+				0xFFFFFFF
+			);
+			Novice::DrawSprite(
+				0, bg1Y,
+				bgGH,
+				1, 1,
+				0.0f,
+				0xFFFFFFF
+			);
+			break;
+		case RESULT:
+			Novice::DrawBox(
+				0, 0,
+				1280, 720,
+				0.0f,
+				0xC1EFC1FF,
+				kFillModeSolid
+			);
+			break;
+		}
 
 		///
 		/// ↑描画処理ここまで
