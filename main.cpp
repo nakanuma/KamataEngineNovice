e1b86488f5a1009b7062b8b203e54b87b236d33f
k023g0121desktop <k023g0121@g.neec.ac.jp>
2023-05-05 19:41:14 +0900

    応用課題　途中

--- main.cpp
+++ main.cpp
@@ -22,10 +22,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int playerX = 640;
 	int playerY = 360;
 
+	//弾の最大数
+	const int bulletNum = 8;
+
+	//弾の初期位置と半径
+	int bulletX[bulletNum];
+	int bulletY[bulletNum];
+	int bulletR = 8;
+
+	//弾の速度
+	int bulletSpd = 8;
+
+	//弾のフラグ
+	bool isBulletShot[bulletNum] = { false };
+
 	//画像読み込み
 	int player = Novice::LoadTexture("./images/player.png");
-	/*int bullet = Novice::LoadTexture("./images/bullet.png");*/
+	int bullet = Novice::LoadTexture("./images/bullet.png");
 
+	//弾のカウント
+	int i;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -50,6 +66,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		playerY = min(max(playerY, playerR), 720 - playerR);
 
+		//スペースキーが押されたら弾を発射
+		if (Novice::CheckHitKey(DIK_SPACE)) {
+			for (i = 0; i <= bulletNum; i++) {
+				if (!isBulletShot[i]) {
+					isBulletShot[i] = true;
+					bulletX[i] = playerX;
+					bulletY[i] = playerY;
+					break;
+				}
+			}
+		}
+
+		//弾の発射中、弾を上方向へ移動させて画面外に出たら弾フラグをfalseに
+		for (i = 0; i <= bulletNum; i++) {
+			if (isBulletShot[i]) {
+				bulletY[i] -= bulletSpd;
+				if (bulletY[i] < 0) {
+					bulletY[i] = -200;
+					isBulletShot[i] = false;
+				}
+			}
+		}
 
 		///
 		/// ↑更新処理ここまで
@@ -58,6 +96,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+	
+		//弾を発射中、弾を描画
+		for (i = 0; i <= bulletNum; i++) {
+			Novice::DrawSprite(bulletX[i] - bulletR, bulletY[i] - bulletR, bullet, 1, 1, 0.0f, 0xFFFFFFFF);
+		}
 
 		//自機の描画
 		Novice::DrawSprite(playerX - playerR, playerY - playerR, player, 1, 1, 0.0f, 0xFFFFFFFF);
@@ -69,8 +112,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//自機の座標を表示
 		Novice::ScreenPrintf(0, 60, "PlayerPosX:%5d      PlayerPosY:%5d", playerX, playerY);
 
-		///
+		//弾のクールタイムを表示
+		Novice::ScreenPrintf(0, 80, "bulletCoolTime: unko");
 
+		//弾のフラグを表示
+		for (i = 0; i < 8; i++) {
+			Novice::ScreenPrintf(0, 100 + i * 20, "isBulletShot[%d]:%2d", i, isBulletShot[i]);
+		}
+		
 		///
 		/// ↑描画処理ここまで
 		///
