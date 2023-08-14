a9f8d65ea69867f541dc216fe3b5c9a479d7b1ba
k023g0121desktop <k023g0121@g.neec.ac.jp>
2023-08-14 11:28:10 +0900

    シーン遷移は完成

--- Easing.cpp
+++ Easing.cpp
@@ -13,3 +13,16 @@ float EaseInQuartPos(float start, float end, float t)
 	return start + distance;
 }
 
+float EaseInQuad(float t)
+{
+	return t * t;
+}
+
+float EaseInQuadPos(float start, float end, float t)
+{
+	float distance = end - start;
+	distance *= EaseInQuad(t);
+
+	return start + distance;
+}
+
--- Easing.h
+++ Easing.h
@@ -3,3 +3,7 @@
 float EaseInQuart(float t);
 
 float EaseInQuartPos(float start,float end, float t);
+
+float EaseInQuad(float t);
+
+float EaseInQuadPos(float start, float end, float t);
